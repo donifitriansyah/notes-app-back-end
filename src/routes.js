109f81082c899234954ca627887813abe1e0f78f
Doni Fitriansyah <102551960+donifitriansyah@users.noreply.github.com>
2024-10-10 22:51:38 +0700

    fungsi get

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
-const { addNoteHandler } = require('./handler');
+const { addNoteHandler, getAllNotesHandler } = require('./handler');
 const routes = [
   {
     method: 'POST',
     path: '/notes',
     handler: addNoteHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
     options: {
       cors: {
         origin: ['*'],
